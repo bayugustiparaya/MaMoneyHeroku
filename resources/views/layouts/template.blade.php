e74bf26cc818e51a4b10f72964207f992a59885e
bayugustiparaya <bayu.gustip7820@gmail.com>
2021-05-28 23:28:26 +0700

    add meta

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title>{{ config('app.name', 'MyMoney') }} | @yield('title')</title>
     <!-- Fonts -->
     <link href="{{ asset('tampilan/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
